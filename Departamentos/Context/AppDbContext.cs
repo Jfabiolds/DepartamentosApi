519aa109742a5c61c36d2b09d040884610349b17
jfabi <jfabi@DESKTOP-MGQASMN>
2022-03-11 20:59:34 -0300

    Adicionar arquivos de projeto.

---
+++ Departamentos/Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Departamentos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Departamentos.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Departamento> Departamentos { get; set; }
+        public DbSet<Funcionario> Funcionarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Integrated Security=SSPI;Persist Security Info=False;User ID=DESKTOP-MGQASMN\jfabi;Initial Catalog=ProjetoA;Data Source=DESKTOP-MGQASMN\SQLEXPRESS
+");
+        }
+    }
+}
---
+++ Departamentos/Models/Funcionario.cs
@@ -0,0 +1,13 @@
+﻿namespace Departamentos.Models
+{
+    public class Funcionario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Foto { get; set; }
+        public decimal RG { get; set; }
+        public Departamento Departamento { get; set; }
+        
+
+    }
+}
